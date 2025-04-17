d99a059792157b325d2d653a428366939dbb72fb
k.seretis <serkleanthis@gmail.com>
2025-04-17 16:52:30 +0300

    Db schema update

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -1,14 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 using TheUltimateStrictLibrary.DataTypes;
 
 namespace Domain.Models;
 
+[Table("Users", Schema = "mainApp")]
 public class User
 {
     [Key]
     public int Id { get; set; }
 
+    [Comment("The name is passed through the Name class from the 'TheUltimateStrictLibrary' package")]
     public required Name Name { get; set; }
 
     [Column(TypeName = "varchar(255)")]
--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -9,6 +9,8 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.HasDefaultSchema("mainApp");
+
         modelBuilder.Entity<User>().OwnsOne(u => u.Name, name =>
         {
             name.Property(n => n.Value).HasColumnName("Name").IsRequired().HasMaxLength(100);
