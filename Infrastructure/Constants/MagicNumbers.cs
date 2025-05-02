8a5c382bf44610c010deb95cda327a850c56dfdc
k.seretis <serkleanthis@gmail.com>
2025-05-02 13:21:14 +0300

    Small changes

---
+++ Infrastructure/Constants/MagicNumbers.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Helpers;
+
+public abstract class MagicNumbers
+{
+    public const int EmailLength = 100;
+    public const int PhoneNumberLength = 13;
+    public const int NameLength = 100;
+}
--- Infrastructure/Helpers/MagicNumbers.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Infrastructure.Helpers;
-
-public interface MagicNumbers
-{
-    const int EmailLength = 100;
-    const int PhoneNumberLength = 13;
-    const int NameLength = 100;
-}
